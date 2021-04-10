b7d38c83faf1bbf105efa9fcfd752d91764d3827
Mayank Sharma <mayank.sharma.uk@gmail.com>
2021-04-10 22:46:30 +0100

    IMDB search added

--- functions/IMDB/IMDB.ts
+++ functions/IMDB/IMDB.ts
@@ -4,9 +4,8 @@ import axios from "axios";
 export default class implements Action {
 
     async preProcessing(args: string[]) {
-        const xmlFetch = await axios.get(`http://api.themoviedb.org/3/search/movie?query=${args.join(" ")}&api_key=4fecdf0b54d07c822019cc3591d88546`)
-        console.log(xmlFetch.data)
-        return {movie: xmlFetch.data.results[0]}
+        const imdbResults = await axios.get(`http://api.themoviedb.org/3/search/movie?query=${args.join(" ")}&api_key=4fecdf0b54d07c822019cc3591d88546`)
+        return {movie: imdbResults.data.results[0]}
     }
 
 }
