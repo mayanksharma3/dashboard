eea7e0c43042ecd2259e0abb6b7a1f35cf411ceb
Mayank Sharma <mayank.sharma.uk@gmail.com>
2021-04-12 09:22:35 +0100

    Update variables fetching

--- functions/NEWS/news.ts
+++ functions/NEWS/news.ts
@@ -5,10 +5,11 @@ const Feed = require('rss-to-json');
 export default class extends Action {
 
     async preProcessing(args: { id: string, variables: {[key: string]: string }}) {
+        console.log(args)
         let query = ""
         const result: any = {}
         if (args.id === "querySearch") {
-            result.query = args["QUERY"]
+            result.query = args.variables["QUERY"]
             query = "?q=" + result.query
         }
         result.news = await Feed.load('https://news.google.com/rss' + query);
